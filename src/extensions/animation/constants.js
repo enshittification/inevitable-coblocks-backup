82742c53286d72fbcbd7f9427074745ce796d02e
Bart Kalisz <bartlomiej.kalisz@gmail.com>
2021-08-24 13:55:48 +0200

    Remove unused extensions

--- src/extensions/animation/constants.js
+++
@@ -1,1 +0,0 @@
-export const ANIMATION_FEATURE_ENABLED_KEY = 'coblocks_animation_controls_enabled';
--- src/extensions/colors/classes.js
+++
@@ -1,7 +0,0 @@
-function ColorSettingsClasses( props ) {
-	return [
-		{ 'has-text-color': props.attributes.textColor || props.attributes.customTextColor },
-	];
-}
-
-export default ColorSettingsClasses;
